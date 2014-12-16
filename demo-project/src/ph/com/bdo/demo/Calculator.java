12c847dbc07367ac6c85874583dba0bd4abaffdc
Richie Panganiban <richie.panganiban@orangeandbronze.com>
2014-12-16 11:15:40 +0800

    Add initial project base for demo-project

---
+++ demo-project/src/ph/com/bdo/demo/Calculator.java
@@ -0,0 +1,9 @@
+package ph.com.bdo.demo;
+
+public class Calculator {
+
+	public void addAndPrintNumbers(int x, int y) {
+		int sum = x + y;
+		System.out.println(sum);
+	}
+}
---
+++ demo-project/src/ph/com/bdo/demo/Main.java
@@ -0,0 +1,9 @@
+package ph.com.bdo.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.addAndPrintNumbers(1, 1);
+	}
+}
