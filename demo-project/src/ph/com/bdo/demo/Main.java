f3f3d26329e34bb3e50e6abcf8f442a5cc93fcf6
Crisar Mark Dolorica <crisardolorica09@gmail.com>
2014-12-17 17:19:51 +0800

    Updated multiplyAndPrintNumbers method to Bigdecimal operator

--- demo-project/src/ph/com/bdo/demo/Main.java
+++ demo-project/src/ph/com/bdo/demo/Main.java
@@ -1,11 +1,13 @@
 package ph.com.bdo.demo;
 
+import java.math.BigDecimal;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
 		calculator.addAndPrintNumbers(1, 1);
-		calculator.multiplyAndPrintNumbers(9, 12);
+		calculator.multiplyAndPrintNumbers(new BigDecimal("1.1"),new BigDecimal("2.3"));
 		calculator.divideAndPrintNumbers(10, 10);
 	}
 }
